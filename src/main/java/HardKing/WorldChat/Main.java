211678b6d52cc1672a7919c9e061c28678450a40
OğuzhanK <54169162+hardsiamang655@users.noreply.github.com>
2022-01-16 21:39:43 +0300

    Update Main.java

--- src/main/java/HardKing/WorldChat/Main.java
+++ src/main/java/HardKing/WorldChat/Main.java
@@ -1,1 +1,18 @@
-
+/*
+ _    _               _ _  ___             
+| |  | |             | | |/ (_)            
+| |__| | __ _ _ __ __| | ' / _ _ __   __ _ 
+|  __  |/ _` | '__/ _` |  < | | '_ \ / _` |
+| |  | | (_| | | | (_| | . \| | | | | (_| |
+|_|  |_|\__,_|_|  \__,_|_|\_\_|_| |_|\__, |
+                                      __/ |
+                                      |___/ 
+*@author HardKing (HardFighter655)
+*
+*@Hard Development STUDİOS Tüm Hakları Saklıdır!
+*
+*@Eklenti HardKing Tarafından Özel Olarak Kodlanmıştır. Dağıtılması Kesinlikle Yasaktır!
+*
+*@YouTube - HARD (oyun yönetmeni)
+*
+*/
